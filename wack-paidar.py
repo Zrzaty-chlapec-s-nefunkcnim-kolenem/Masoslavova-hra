1ba3a53a77e98957f851dc0932e31fe7010c26b5
Masoslav Ukrajinko <dao.dom64@gmail.com>
2025-06-18 23:50:48 +0200

    Restart + výhra a prohra

--- wack-paidar.py
+++ wack-paidar.py
@@ -8,7 +8,7 @@ damage_2 = pygame.mixer.Sound("damage2.mp3")
 vyhra = pygame.mixer.Sound("výhra.mp3")
 prohra =pygame.mixer.Sound("prohra.mp3")
 prohra2 =pygame.mixer.Sound("prohra2.mp3")
-
+vyhra2 = pygame.mixer.Sound("výhra2.mp3")
 # herní okno
 window_width = 600
 window_height = 600
@@ -21,7 +21,7 @@ background_image = pygame.image.load("gchd.jpg").convert()
 background_image = pygame.transform.scale(background_image, (window_width, window_height))
 clock = pygame.time.Clock()
 text_font = pygame.font.Font("PixelifySans.ttf",50)
-grid = [(x, y) for x in range(0, window_width, cell_size) for y in range(0, window_height, cell_size)]
+grid = [(x, y) for x in range(0, window_width, cell_size) for y in range(cell_size, window_height, cell_size)]
 object_size = 120
 object_offset = (cell_size - object_size) // 2
 
@@ -56,6 +56,7 @@ score = 0
 timer = pygame.time.get_ticks()
 visible_time = 450
 game_active = False
+win = False
 streak = 0
 game_over = False
 miss_streak = 0
@@ -63,6 +64,21 @@ miss_streak = 0
 # Herní smyčka
 while True:
     for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and (game_over or win):
+                score = 0
+                timer = pygame.time.get_ticks()
+                visible_time = 450
+                game_active = True
+                game_over = False
+                win = False
+                streak = 0
+                miss_streak = 0
+                mole.new_position()
+                pygame.mixer.stop()
+                bg_music.play(-1)
+
+
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
@@ -82,55 +98,70 @@ while True:
                 else:
                     streak = 0
                     miss_streak += 1
-                    if miss_streak >= 10:
-                        game_active = False
-                        game_over = True
-                        bg_music.stop()
-                        prohra.play()
-                        prohra2.play()
-    else:
-        game_active = True
-        if not pygame.mixer.get_busy():
-            bg_music.play(-1)
     if game_active:
-        if score < 5:
-            visible_time = 800
-        
-        elif score < 10:
-            visible_time = 600
-          
-        elif score < 15:
-            visible_time = 450
-
-        else:
-            visible_time = 300
-            
-        screen.blit(background_image, (0, 0))
-       
-        holes.draw(screen)
-        mole_group.draw(screen)
-
         if pygame.time.get_ticks() - timer > visible_time:
+            miss_streak += 1
+            streak = 0
             mole.new_position()
             timer = pygame.time.get_ticks()
 
-        score_surface = text_font.render(f"Skóre: {score}", True, (255, 255, 255))
+        if score >= 25:
+            game_active = False
+            win = True
+            bg_music.stop()
+            vyhra.play()
+            vyhra2.play(-1)
+        if miss_streak >= 20:
+            game_active = False
+            game_over = True
+            bg_music.stop()
+            prohra.play()
+            prohra2.play(-1)
+
+    else:
+        if not game_over and not win and not pygame.mixer.get_busy():
+            bg_music.play(-1)
+        if not game_over and not win:
+            game_active = True
+
+    screen.blit(background_image, (0, 0))
+
+    if game_active:
+        if score < 5:
+            visible_time = 800
+        elif score < 10:
+            visible_time = 600
+        elif score < 15:
+            visible_time = 450
+        else:
+            visible_time = 350
+
+        holes.draw(screen)
+        mole_group.draw(screen)
+
+        score_surface = text_font.render(f"Skóre: {score}", True, (255, 255, 255), (0, 0, 0))
         screen.blit(score_surface, (20, 20))
 
     elif game_over:
-        screen.blit(background_image, (0, 0))
+        game_over_text = text_font.render("PAIDLER ZNIČIL ŠKOLU", True, (255, 0, 0))
+        screen.blit(game_over_text, (window_width // 2 - game_over_text.get_width() // 2, 40))
+        icon_scaled = pygame.transform.scale(icon, (700, 700))
+        icon_rect = icon_scaled.get_rect(center=(window_width // 2, window_height // 2))
+        screen.blit(icon_scaled, icon_rect)
+        restart_text = text_font.render("MEZERNÍK = restart", True, (255, 255, 255))
+        screen.blit(restart_text, (window_width // 2 - restart_text.get_width() // 2, 100))
 
-        game_over_img = pygame.image.load("logo.png").convert_alpha()
-        game_over_img = pygame.transform.scale(game_over_img, (300, 300))  # zmenšení loga
-        screen.blit(game_over_img, (
-            window_width // 2 - game_over_img.get_width() // 2,
-            window_height // 2 - game_over_img.get_height() // 2
-        ))
 
-        game_over_text = text_font.render("PAIDAR ZNIČIL ŠKOLU", True, (255, 0, 0))
-        screen.blit(game_over_text, (
-            window_width // 2 - game_over_text.get_width() // 2,
-            40
-        ))
+    elif win:
+        win_text = text_font.render("ZACHRÁNIL SI ŠKOLU!", True, (0, 255, 0))
+        screen.blit(win_text, (window_width // 2 - win_text.get_width() // 2, 40))
+        icon_scaled = pygame.transform.scale(icon, (700, 700))
+        icon_rect = icon_scaled.get_rect(center=(window_width // 2, window_height // 2))
+        screen.blit(icon_scaled, icon_rect)
+        restart_text = text_font.render("MEZERNÍK = restart", True, (255, 255, 255))
+        screen.blit(restart_text, (window_width // 2 - restart_text.get_width() // 2, 100))
+
+
     pygame.display.update()
     clock.tick(60)
+ 
